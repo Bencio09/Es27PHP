faa54f0d96f2b19ce92664c2e26823195763df38
Bencio09 <lorebencini13@outlook.com>
2022-03-24 13:46:22 +0100

    Commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include "./Pages/connessione.php";
+
+?>
+
+<html>
+
+<head>
+    <title>Votazioni</title>
+    <link rel="stylesheet" href="./CSS/bootstrap.css" type="text/css">
+    <script src="./JS/bootstrap.js"></script>
+</head>
+
+<body style="background-color: orange;">
+    <h3 style="text-align: center;">BENVENUTI NEL NUOVO SISTEMA ELETTORALE ELETTRONICO </h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-5" style="background-color: gray; border: yellow solid 5px;">
+                    Attraverso questo innovativo sistema è possibile adesso indicare la propria preferenza
+                    di voto scegliendo uno dei candidati proposti da un menu a scorrimento.
+                    La definitiva conferma della scelta operata equivale alla consegna della tradizionale
+                    scheda elettorale al presidente di seggio: presti pertanto molta attenzione alle operazioni
+                    che sarà chiamato a svolgere per giungere alla compilazione della Sua scheda di voto e
+                    alla conferma della Sua effettiva scelta.</p>
+                </div>
+                <div class="col-3">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-5" style="background-color: greenyellow; border: yellow solid 5px;">
+                    <p>L'operazione di voto si svolge in tre parti</p>
+                    <ul>
+                        <li>Selezione della lista elettorale da una casella compìbinata</li>
+                        <li>Selezione del candidato appartenente alla suddetta lista</li>
+                        <li>Conferma del voto o ritorno alla prima fase</li>
+                    </ul>
+                </div>
+                <div class="col-3">
+                </div>
+            </div>
+        </div>
+
+
+</body>
+
+</html>
