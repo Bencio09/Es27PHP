76fdcf6f3402c6b47b0ec223b02c51bb539e14c3
Bencio09 <lorebencini13@outlook.com>
2022-05-05 12:26:07 +0200

    Iniziata parte 2

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
     session_start();
+    include "./Pages/connessione.php";
     
+
 ?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="./CSS/bootstrap.css" type="text/css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <title>Login</title>
+    </head>
+    <body>
+        
+    </body>
+
+</html>
